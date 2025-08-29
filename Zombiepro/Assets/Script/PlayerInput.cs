f785aa7949d7b2b9ceebc3cd82237c8f6facee98
leejaeyoung <kltop1210@gmail.com>
2025-08-29 11:14:00 +0900

    commit

---
+++ Zombiepro/Assets/Script/PlayerInput.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string moveAxisName = "Vertical";
+    public string fireAxisName = "Fire1";
+    public string reloadAxisName = "Reload";
+
+    public float move { get; private set; }
+    public float fire { get; private set; }
+    public float reload { get; private set; }
+
+    private void Update()
+    {
+        
+    }
+
+ 
+
+
+
+
+}
